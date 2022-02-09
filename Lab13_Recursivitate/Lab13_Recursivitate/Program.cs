a055eb690d01fc85bb78dc5e68b88ffc81acc837
Andrei Androne <andrei.androne@outlook.com>
2022-02-09 20:17:27 +0200

    Lab13_Recursivity OopFriendAssembly

---
+++ Lab13_Recursivitate/Lab13_Recursivitate/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Lab13_Recursivitate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new[] { 1, 2, 3, 4, 5 };
+            int element = 5;
+            int index = RecursivityHelper.IndexOfElement(element, array);
+            Console.WriteLine($"Element {element} found at index: {index}");
+        }
+
+        private static void Example_Factorial()
+        {
+            int n = 5;
+            int result = RecursivityHelper.Factorial(n);
+            Console.WriteLine($"Factorial({n}) = { result}");
+        }
+
+        private static void Example_PrintArray()
+        {
+            RecursivityHelper.PrintArray(new [] { 1, 2, 3, 4, 5 });
+        }
+
+        private static void Example_Fibonacci()
+        {
+            int n = 6;
+            long result = RecursivityHelper.Fibonacci(n);
+            Console.WriteLine($"Fibonacci({n}): {result}");
+            Console.WriteLine($"Fibonacci({n}) calculated in {RecursivityHelper.fiboCallCount} steps.");
+        }
+    }
+}
+
---
+++ Lab13_Recursivitate/Lab13_Recursivitate/RecursivityHelper.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+namespace Lab13_Recursivitate
+{
+    public static class RecursivityHelper
+    {
+        internal static int fiboCallCount = 0;
+        private static long[] FiboCache = new long[1000];
+
+        public static int Factorial(int n)
+        {
+            if (n == 1 || n == 0)
+            {
+                return 1;
+            }
+
+            return n * Factorial(n - 1);
+        }
+
+        public static void PrintArray(int[] array)
+        {
+            if(array is null || array.Length == 0)
+            {
+                return;
+            }
+
+            PrintArray(array, 0);
+        }
+
+        private static void PrintArray(int[] array, int index)
+        {
+            Console.Write(array[index]);
+
+            if (index >= 0 && index < array.Length-1)
+            {
+                Console.Write(", ");
+                PrintArray(array, index + 1);
+            }
+        }
+
+        public static long Fibonacci(int n)
+        {
+            if (n == 0 || n == 1)
+            {
+                return n;
+            }
+
+            if (FiboCache[n] > 0)
+            {
+                return FiboCache[n];
+            }
+
+            fiboCallCount++;
+
+            long result = Fibonacci(n - 1) + Fibonacci(n - 2);
+
+            FiboCache[n] = result;
+
+            return result;
+        }
+
+        public static int IndexOfElement(int element, int[] array)
+        {
+            return IndexOfElement(element, array, 0, array.Length - 1);
+        }
+
+        private static int IndexOfElement(int element, int[] array, int indexStart, int indexEnd)
+                                           {
+            int middleIndex = (indexStart + indexEnd) / 2;
+
+            if(element == array[middleIndex])
+            {
+                return middleIndex;
+            }
+            else if (element < array[middleIndex])
+            {
+                //we need to search in the left sub array
+                return IndexOfElement(element, array, indexStart, middleIndex - 1);
+            }
+            else
+            {
+                //we need to search in the right sub array
+                return IndexOfElement(element, array, middleIndex + 1, indexEnd);
+            }
+        }
+    }
+}
